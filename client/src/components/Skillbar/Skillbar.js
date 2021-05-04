5832473239671e97f2ae3cf3cbc67be298877dc4
Hasnat Farooq <hassnaatfarooq@gmail.com>
2021-05-04 14:13:09 +0100

    improve project and contact screen and skillbar component

--- client/src/components/Skillbar/Skillbar.js
+++ client/src/components/Skillbar/Skillbar.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Skillbar.css';
+import React from "react";
+import "./Skillbar.css";
 
-const Skillbar = ({name,percentage}) => {
-    const perc = Number(percentage)
-    return (
-            <div className='skill'>
-                    <h4 className='skill__name'>{name}</h4>
-                    <div className='skillbar'>
-                        <div className='progress' style={{width:`${perc}%`}}></div>
-                    </div>
-            </div>
-    )
-}
+const Skillbar = ({ name, percentage }) => {
+  const perc = Number(percentage);
+  return (
+    <div className="skill">
+      <h4 className="skill__name">{name}</h4>
+      <div className="skillbar">
+        <div className="progress" style={{ width: `${perc}%` }}></div>
+      </div>
+    </div>
+  );
+};
 
-export default Skillbar
+export default Skillbar;
